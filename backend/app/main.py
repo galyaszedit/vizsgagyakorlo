6fe71db10410e232647fd446a7c62e0cf0b9e441
[Edit Galyasz] <[galyaszedit@gmail.com]>
2026-01-07 14:45:32 +0100

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://*.onrender.com"],
+    allow_origins=["http://localhost:5173", "https://vizsgagyakorlo-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
